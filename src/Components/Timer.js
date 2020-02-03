6ab2ee0fbf5fbe19c0f1e3ef88bbd7c2014d15e0
oseiasal <oseiasal@gmail.com>
2020-02-03 16:16:30 -0300

    refactoring function to class

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React from 'react'
 
-function Timer(props) {
-  return (
+class Timer extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      minutes: 0,
+      starts: 0,
+      isOn:false
+    }
+  }
 
-<div className="timer-container">
-  <div className="timer-numbers">
-    <span>00:</span><span>{props.time}</span>
-  </div>
-</div>
-  );
+  render () {
+    return (
+      <div className="timer-container">
+      <div className="timer-numbers">
+      <span>00:</span><span>{this.props.time}</span>
+      </div>
+      </div>
+    )
+  }
 }
-
 
 export default Timer;
