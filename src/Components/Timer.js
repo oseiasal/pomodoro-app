fefbe291d4b9e69993e2abc1c074b98ad1728707
oseiasal <oseiasal@gmail.com>
2020-02-05 19:15:31 -0300

    handleminutes configurado

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -4,35 +4,48 @@ class Timer extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      starts: 0,
-      isOn:false,
+      minutes: 10,
+      isOn: false
     }
   }
-  
-  checkTimer = () => {
 
+  handleMinutes = () => {
+    const count = this.state.minutes
+    let i = 10
+    let timer = setInterval(() => {
+      i--
+      if(i < 0) {
+        this.setState({minutes: 10})
+
+        // TODO: Quando o app chegar a 0, tornar o isOn falso
+        clearInterval(timer)
+      } else {
+
+        this.setState(state => {
+          return {minutes: state.minutes - 1}
+        })
+
+      }
+
+    }, 1000)
+
+
+  }
+
+  componentWillReceiveProps(a){
+    if(a.isOn == true) {
+      this.handleMinutes.call()
+    }
   }
 
   render () {
     return (
       <div className="timer-container">
       <div className="timer-numbers">
-      <span>00:</span><span>{this.state.isOn == false ? 0 : this.props.time}</span>
+      <span>00:</span><span>{this.state.minutes}</span>
       </div>
       </div>
     )
-  }
-
-  componentDidMount() {
-    var timer =  setInterval(() => {
-      this.setState(state => {
-        return {minutes: state.minutes - 1}
-      })
-    },1000)
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timer)
   }
 }
 
