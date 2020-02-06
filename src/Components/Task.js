1251cb8d32cc9edbc5221830bc87eb5e515414f9
oseiasal <oseiasal@gmail.com>
2020-02-06 18:22:03 -0300

    clean component

--- src/Components/Task.js
+++ src/Components/Task.js
@@ -1,26 +1,17 @@
 import React from 'react';
 
-class Task extends React.Component{
-  constructor (props) {
-    super(props);
-  }
-
-  oi = () => {
-    console.log('oi');
-  }
-
-  render () {
-    return (
-    <div>
-      <div className="task-container">
-        <div className="task-box">
-          <input  placeholder="Digite o nome da tarefa" required id="text" type="text"/>
-          <input type="button" onClick={this.props.funcion} value="Enviar"/>
-        </div>
+function Task (props) {
+  return (
+  <div>
+    <div className="task-container">
+      <div className="task-box">
+        <input  placeholder="Digite o nome da tarefa" required id="text" type="text"/>
+        <input type="button" onClick={props.funcion} value="Enviar"/>
       </div>
     </div>
-    );
-  }
+  </div>
+  );
 }
+
 
 export default Task
