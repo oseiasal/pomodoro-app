a6e979206d7a2abf3b4239af8a04631a26caf6a5
oseiasal <oseiasal@gmail.com>
2020-02-13 14:32:43 -0300

    Updates

---
+++ precache-manifest.1971781f6289e45bafa394df5ccb8d16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dece571194fd0ffde9dd917c1d961e49",
+    "url": "/pomodoro-timer/index.html"
+  },
+  {
+    "revision": "4f961b1e6839491b7b18",
+    "url": "/pomodoro-timer/static/css/main.7cbae81f.chunk.css"
+  },
+  {
+    "revision": "07203eb4fca11240c541",
+    "url": "/pomodoro-timer/static/js/2.d987b3ad.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/pomodoro-timer/static/js/2.d987b3ad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4f961b1e6839491b7b18",
+    "url": "/pomodoro-timer/static/js/main.3bb8b7ad.chunk.js"
+  },
+  {
+    "revision": "3870920692bd11326093",
+    "url": "/pomodoro-timer/static/js/runtime-main.da7e7365.js"
+  }
+]);
