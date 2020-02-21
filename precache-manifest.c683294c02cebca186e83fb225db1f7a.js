2e15bf44d8091f6010f70cf134cb822d428244e9
oseiasal <oseiasal@gmail.com>
2020-02-21 16:12:02 -0300

    Updates

---
+++ precache-manifest.c683294c02cebca186e83fb225db1f7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "258ef700198305e11c76412fbe46c651",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c634a007833315da0e3f",
+    "url": "./static/css/main.7cbae81f.chunk.css"
+  },
+  {
+    "revision": "3a72801af3d5584dc9f2",
+    "url": "./static/js/2.e115427b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.e115427b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c634a007833315da0e3f",
+    "url": "./static/js/main.dd302898.chunk.js"
+  },
+  {
+    "revision": "5fc7312d2fff012b5d8c",
+    "url": "./static/js/runtime-main.e6033449.js"
+  }
+]);
--- src/redux/reducers/index.js
+++
@@ -1,7 +0,0 @@
-// import
-import { reducerItem } from './reducerItem'
-import { reducerTimer } from './reducerTimer'
-import { combineReducers } from 'redux'
-
-export const rootReducers = combineReducers({reducerItem, reducerTimer});
-
